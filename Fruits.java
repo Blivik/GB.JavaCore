05c0c0211cef379af0eba460fb1dac96f5baa9fc
Blivernitz Victoria <73316978+Blivik@users.noreply.github.com>
2021-10-26 23:52:57 +0200

    JC.Lesson3

---
+++ Fruits.java
@@ -0,0 +1,13 @@
+package Gb.company;
+
+public class Fruits {
+    private double weight;
+
+    public Fruits(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
